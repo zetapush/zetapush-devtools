c1b1c267e822d6b5c6cd18bb145a7f5ee0d85415
Grégory Houllier <gregory.houllier@gmail.com>
2017-10-17 17:41:14 +0200

    feat(build) Update build script

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,21 @@
 
 set -e
 
-echo '[LOG] Build application'
-yarn run build:optimized --base-href=/
-echo '[LOG] Clean previous packaged applications'
-rm -f devtools.zpush.io.tar.gz
-echo '[LOG] Package application'
-cd ./dist
-tar cvfz ../devtools.zpush.io.tar.gz ./**
-echo '[LOG] Deploy application on webserver'
-rsync -azv ./** ubuntu@devtools.zpush.io:/var/www/devtools.zpush.io/
+hash=$(git rev-parse --verify HEAD)
+artifact="devtools.zpush.io--$hash.tar.gz"
+file=./archives/$artifact
+
+if [ -f $file ]; then
+   echo "[LOG] Artifact already exists"
+else
+  echo "[LOG] Build application"
+  yarn run build:optimized --base-href=/
+  echo "[LOG] Clean previous packaged applications"
+  rm -f $file
+  echo "[LOG] Package application"
+  cd ./dist
+  echo "[LOG] Generate artifact $artifact"
+  tar cvfz .$file ./**
+  echo "[LOG] Deploy application on webserver"
+  rsync -azv ./** ubuntu@devtools.zpush.io:/var/www/devtools.zpush.io/
+fi
