c18e36008bff993bdb292c55a84826c6a73cd099
Grégory Houllier <gregory.houllier@gmail.com>
2017-10-26 00:25:10 +0200

    feat(build) Update targeted source as es2017

--- src/polyfills-aot.ts
+++ src/polyfills-aot.ts
@@ -18,10 +18,6 @@
  * BROWSER POLYFILLS
  */
 
-/** Evergreen browsers require these. **/
-import 'core-js/es6/reflect';
-import 'core-js/es7/reflect';
-
 /***************************************************************************************************
  * Zone JS is required by Angular itself.
  */
