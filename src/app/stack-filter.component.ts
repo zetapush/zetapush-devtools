d586398dc4e9065232c006249b652f73429f28dd
Anthony Fertil <anthony.fertil@hotmail.fr>
2018-01-22 18:14:00 +0100

    Create a filter component for stack traces to reuse it in the terminal view

---
+++ src/app/stack-filter.component.ts
@@ -0,0 +1,84 @@
+import {
+  Component,
+  Input,
+  OnDestroy,
+  OnChanges,
+  Output,
+  EventEmitter,
+  ChangeDetectionStrategy,
+  SimpleChanges,
+} from '@angular/core';
+import { MatCheckboxChange } from '@angular/material';
+import {
+  Trace,
+  TraceCompletion,
+  TraceLocation,
+  parseTraceLocation,
+} from './trace.interface';
+import {} from '@angular/core/src/metadata/lifecycle_hooks';
+
+interface ViewTypeFilter {
+  label: string;
+  selected: boolean;
+}
+
+@Component({
+  selector: 'zp-stack-filter',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  styles: [
+    `
+    .Form--Filter {
+      font-weight: bold;
+      margin-top: 10px;
+      padding-left: 0.5rem;
+    }
+    .Filter {
+      padding-right: 0.25rem;
+    }
+    `,
+  ],
+  template: `
+    <form class="Form Form--Filter">
+      <span>Filter: </span>
+      <mat-checkbox *ngFor="let type of types" [checked]="type.selected" (change)="onChangeType($event, type)" name="filter" class="Filter">
+        {{type.label}}
+      </mat-checkbox>
+    </form>
+  `,
+})
+export class StackFilterComponent implements OnChanges {
+  @Input() traces: Trace[] = [];
+
+  @Output() filteredTraces = new EventEmitter<Trace[]>();
+
+  types: ViewTypeFilter[] = [
+    { label: 'MS', selected: false },
+    { label: 'ME', selected: false },
+    { label: 'CMT', selected: false },
+    { label: 'USR', selected: true },
+  ];
+
+  ngOnChanges(changes: SimpleChanges) {
+    this.filteredTraces.emit(this.filtered);
+  }
+
+  get filtered() {
+    const types = this.types
+      .filter(type => type.selected)
+      .map(type => type.label);
+    return this.traces
+      ? this.traces.filter(trace => types.includes(trace.type))
+      : [];
+  }
+
+  onChangeType($event: MatCheckboxChange, type) {
+    this.types = this.types.map(value => {
+      if (value.label === type.label) {
+        value.selected = $event.checked;
+      }
+      return value;
+    });
+
+    this.filteredTraces.emit(this.filtered);
+  }
+}
